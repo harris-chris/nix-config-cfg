456a06e4f0881857b13fc2eb8d3d35170e9928cf
harris-chris <chrisharriscjh@gmail.com>
2021-11-03 12:47:30 +0900

    Move to Kernel 5.15

--- configuration.nix
+++ configuration.nix
@@ -21,6 +21,7 @@ in {
   # Use the systemd-boot EFI boot loader.
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
+  boot.kernelPackages = pkgs.linuxPackages_latest;
 
   networking.hostName = "nixos"; # Define your hostname.
   networking.useDHCP = false;
