6de7bcded1d764c8b8623d4514462de36ca935a8
harris-chris <chrisharriscjh@gmail.com>
2023-07-26 11:28:25 +0900

    Improve formatting for i3status-rust

--- home-manager/programs/i3status-rust/default.nix
+++ home-manager/programs/i3status-rust/default.nix
@@ -10,7 +10,7 @@
              block = "disk_space";
              path = "/";
              info_type = "available";
-             format = "$used / $total";
+             format = "$used / $total  ";
              alert_unit = "GB";
              warning = 80.0;
              alert = 40.0;
@@ -18,30 +18,30 @@
            }
            {
              block = "memory";
-             format = "$icon $mem_used / $mem_total";
+             format = "$icon $mem_used / $mem_total  ";
              warning_mem = 80.0;
              critical_mem = 95.0;
            }
            {
              block = "cpu";
              interval = 1;
-             format = "$icon $barchart $utilization";
+             format = "$icon $barchart $utilization  ";
            }
            {
              block = "docker";
-             format = "$icon $running / $total";
+             format = "$icon $running / $total  ";
            }
            {
              block = "net";
-             format = "$icon ^icon_net_down $speed_down.eng(prefix:K) ^icon_net_up $speed_up.eng(prefix:K) ";
+             format = "$icon ^icon_net_down $speed_down.eng(prefix:K) ^icon_net_up $speed_up.eng(prefix:K)  ";
            }
            {
              block = "external_ip";
-             format = "$ip $country_code_iso3";
+             format = "$ip $country_code_iso3  ";
            }
            {
              block = "battery";
-             format = "$icon $percentage $time $power";
+             format = "$icon  $percentage $time $power  ";
              warning = 50.0;
              critical = 15.0;
            }
@@ -52,7 +52,7 @@
            {
              block = "time";
              interval = 60;
-             format = "$icon $timestamp.datetime()";
+             format = "  $icon $timestamp.datetime()";
            }
 
         ];
