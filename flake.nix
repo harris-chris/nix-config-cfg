f6e58dcbfcda510b685e5bcee43a46f4998edacc
harris-chris <chrisharriscjh@gmail.com>
2025-07-23 15:28:47 +0900

    Upgrade to nixos-unstable

--- flake.nix
+++ flake.nix
@@ -2,9 +2,9 @@
   description = "Home Manager (dotfiles) and NixOS configurations";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-25.05";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
     home-manager = {
-      url = "github:nix-community/home-manager/release-25.05";
+      url = "github:nix-community/home-manager/master";
       inputs.nixpkgs.follows = "nixpkgs";
     };
     nixos-hardware.url = github:NixOS/nixos-hardware/master;
