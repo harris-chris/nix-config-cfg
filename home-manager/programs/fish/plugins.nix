5dff0fea69f0e8bc82ad0ac8be139f93c1814a8b
harris-chris <chrisharriscjh@gmail.com>
2025-07-23 18:36:52 +0900

    Fix bug with fish

--- home-manager/programs/fish/plugins.nix
+++ home-manager/programs/fish/plugins.nix
@@ -7,12 +7,12 @@ let
       owner  = "oh-my-fish";
       repo   = "theme-bobthefish";
       rev    = "76cac812064fa749ffc258a20398c6f6250860c5";
-      sha256 = "1nvazfyz7vk37nska7yw94kknv8jbsqwzz3ybwbnhsnf98xlir0r";
+      sha256 = "sha256-7nZ25R75WsSPqSmyeJbRQ49cITxL3D5CfyplsixFlY8=";
     };
   };
 in
 {
   theme  = bobthefish;
-  prompt = builtins.readFile "${bobthefish.src.out}/fish_prompt.fish";
+  prompt = builtins.readFile "${bobthefish.src.out}/functions/fish_prompt.fish";
 }
 
